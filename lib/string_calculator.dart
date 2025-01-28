cbf904f92c37c942413cec2bb957408a4317a6c7
pankaj <tyagi.pankaj101@gmail.com>
2025-01-28 12:49:48 +0530

    add logic to return the number if it is not empty.

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,7 +4,7 @@ class StringCalculator{
   int add (String number){
     if(number.isEmpty) return 0;// return 0 for empty string
 
-    return 1;//if string is not empty retrun 1 (to fail the case test case if string not empty)
+    return int.parse(number);// TO return the number
   }
 
 }
