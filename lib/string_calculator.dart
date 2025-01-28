349938a040dc6bc6b91899a4b896f342f2617cb7
pankaj <tyagi.pankaj101@gmail.com>
2025-01-28 12:27:20 +0530

    Step -1 :
    Add string calculator class string_calculator.dart-
    - write a function add which take string as argument and return integer.
    
    Add a test class string_calculator_test.dart

---
+++ lib/string_calculator.dart
@@ -0,0 +1,9 @@
+
+class StringCalculator{
+
+  int add (String number){
+
+    return 0;// to remove error becuase fun will retun some integer value.
+  }
+
+}
---
+++ test/string_calculator_test.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+
+  /*
+  *
+  * step 1 Create a simple String calculator with a method signature
+  * */
+
+  test("test case for step 1", (){
+    //todo- need to write test case
+  });
+}
