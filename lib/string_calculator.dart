bf92d119b390eb80301eff5ff00f452238d2a923
pankaj <tyagi.pankaj101@gmail.com>
2025-01-28 13:08:33 +0530

    add logic to handle new line and return sum
    test result- pass

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,7 +3,7 @@ class StringCalculator{
 
   int add (String number){
     if(number.isEmpty) return 0;// return 0 for empty string
-    var sum = number.split(',').map(int.parse).reduce((a,b)=>a+b);
+    var sum = number.split(RegExp(r'[,\n]')).map(int.parse).reduce((a,b)=>a+b);// add regular expression to split the string
     print("${sum}"); // to print sum on console.
     return sum;
   }
