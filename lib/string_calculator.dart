5d847bea6696b09b8d865f94e593ddf94c96c3d3
pankaj <tyagi.pankaj101@gmail.com>
2025-01-28 12:58:03 +0530

    string_calculator.dart =>add logic to split string and add number.
    Test result- pass

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,8 +3,9 @@ class StringCalculator{
 
   int add (String number){
     if(number.isEmpty) return 0;// return 0 for empty string
-
-    return int.parse(number);// TO return the number
+    var sum = number.split(',').map(int.parse).reduce((a,b)=>a+b);
+    print("${sum}"); // to print sum on console.
+    return sum;
   }
 
 }
